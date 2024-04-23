2b167926cd3a5ad5af55cebc0b06a6ff9ca9254d
Junya Watanabe <Junya.Watanabe@uab.cat>
2024-04-23 17:28:25 +0200

    Handle extension .dat in shape_trainer.py

--- shape_trainer.py
+++ shape_trainer.py
@@ -13,7 +13,7 @@ ap.add_argument("-d", "--dataset", type=str, default='train.xml',
 ap.add_argument("-t", "--test", type=str, default=None,
     help="test data (default = test.xml).if not provided, no testing is done", metavar='')
 ap.add_argument("-o", "--out", type=str, default='predictor',
-    help="output filename (default = predictor)", metavar='')
+    help="output filename (default = predictor); extention '.dat' is appended unless the value ends with it", metavar='')
 ap.add_argument("-th", "--threads", type=int, default=1,
     help="number of threads to be used (default = 1)", metavar='')
 ap.add_argument("-dp", "--tree-depth", type=int, default=4,
@@ -51,16 +51,19 @@ options.oversampling_amount = args['oversampling']
 options.oversampling_translation_jitter = args['jitter']
 options.random_seed = args['random_seed']
 options.lambda_param = args['lambda']
-options.be_verbose = True
+
+outfile_name = args['out']
+if not outfile_name.endswith('.dat'):
+    outfile_name += ".dat"
 
 #Training the model
 train_path = os.path.join('./', args['dataset'])
-dlib.train_shape_predictor(train_path, args['out']+".dat", options)
+dlib.train_shape_predictor(train_path, outfile_name, options)
 print("Training error (average pixel deviation): {}".format(
-    dlib.test_shape_predictor(train_path, args['out']+".dat")))
+    dlib.test_shape_predictor(train_path, outfile_name)))
 
 #Testing the model (if test data was provided)
 if args['test'] is not None:
     test_path = os.path.join('./', args['test'])
     print("Testing error (average pixel deviation): {}".format(
-        dlib.test_shape_predictor(test_path, args['out']+".dat")))
+        dlib.test_shape_predictor(test_path, outfile_name)))
